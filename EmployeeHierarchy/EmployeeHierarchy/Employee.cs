058b737bdd1ef0de22102d75356639cb77c9dd27
ApurvaJ-SI <apurva.jalgaonkar@sportzinteractive.net>
2023-08-15 19:32:19 +0530

    First Commit

---
+++ EmployeeHierarchy/EmployeeHierarchy/Employee.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHierarchy
+{
+    abstract internal class Employee
+    {
+        public string Name { get; set; }
+
+        public int EmpId { get; set; }
+
+        public double Salary { get; set; }
+
+        public double Bonus { get; set; }
+
+        string name;
+        int empId;
+        double salary;
+        double bonus;
+
+        public Employee(string name, int empId, double salary, double bonus)
+        {
+            Name = name;
+            EmpId = empId;
+            Salary = salary;
+            Bonus = bonus;
+        }
+        
+        public override string ToString()
+        {
+            return $"Name = {Name}   EmpId = {EmpId}    Salary = {Salary}    Bonus = {Bonus}";
+        }
+
+        abstract public double CalculateBonus();
+        
+    }
+
+    class Manager : Employee
+    {
+        public Manager(string name, int empid, double salary, double bonus, int projects) : base(name, empid, salary, bonus)
+        {
+            NoOfProjects = projects;
+        }
+        public int NoOfProjects { get; set; }
+        public override double CalculateBonus()
+        {
+            return ((Bonus / 100) * Salary) + 9000;
+        }
+    }
+
+    class Developer : Employee
+    {
+        public Developer(string name, int empid, double salary, double bonus, int tasks): base(name, empid, salary, bonus)
+        {
+            NoOfTasks = tasks;
+        }
+        public int NoOfTasks { get; set; }
+
+        public override double CalculateBonus()
+        {
+            return ((Bonus / 100) * Salary) + 6000;   
+        }
+    }
+
+    class SalesPerson : Employee
+    {
+        public SalesPerson(string name, int empid, double salary, double bonus, int clients):base(name, empid, salary, bonus)
+        {
+            NoOfClients = clients;
+        }
+        public int NoOfClients { get; set; }
+        public override double CalculateBonus()
+        {
+            return ((Bonus / 100) * Salary) + 4000;
+        }
+    }
+
+    
+}
---
+++ EmployeeHierarchy/EmployeeHierarchy/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeeHierarchy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Managers:");
+            Manager m1 = new Manager("Apurva",102,50000,15,4);
+            Manager m2 = new Manager("Riya", 104, 47000, 13.2, 3);
+
+            Console.WriteLine(m1.ToString());
+            Console.WriteLine(m2.ToString());
+
+            Console.WriteLine("Developers:");
+            Developer d1 = new Developer("Dipti",203,40000,12.5, 2);
+            Developer d2 = new Developer("Srushti", 206, 39000, 12.3, 3);
+
+            Console.WriteLine(d1.ToString());
+            Console.WriteLine(d2.ToString());
+
+            Console.WriteLine("SalesPersons:");
+            SalesPerson s1 = new SalesPerson("Aayansh", 304, 35000,10.7,8);
+            SalesPerson s2 = new SalesPerson("Siddhi", 307, 34000, 10.3, 6);
+
+            Console.WriteLine(s1.ToString());
+            Console.WriteLine(s2.ToString());
+        }
+    }
+}
